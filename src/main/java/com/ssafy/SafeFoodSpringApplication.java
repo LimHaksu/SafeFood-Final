dc24d4f35842db17bd6a27875db8ded09e981bcb
KM <sodp2@naver.com>
2019-11-21 14:25:15 +0900

    이동

--- src/main/java/com/ssafy/SafeFoodSpringApplication.java
+++ src/main/java/com/ssafy/SafeFoodSpringApplication.java
@@ -2,12 +2,37 @@ package com.ssafy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SafeFoodSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SafeFoodSpringApplication.class, args);
 	}
 
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.groupName("phone management")
+				.apiInfo(metaData())
+				.select()
+				.paths(PathSelectors.any())
+				.build();
+	}
+	
+	// 일반적인 정보를 생성
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("SafeFood API")
+				.description("학수형!! 경무랑 가즈아~!")
+				.version("1.0").build();
+	}
 }
--- src/main/java/com/ssafy/controller/KyoungMooController.java
+++ src/main/java/com/ssafy/controller/KyoungMooController.java
@@ -5,12 +5,20 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.dto.Food;
+import com.ssafy.dto.Post;
+import com.ssafy.service.BoardService;
 import com.ssafy.service.FoodService;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
@@ -18,14 +26,17 @@ import com.ssafy.service.FoodService;
 public class KyoungMooController {
 
 	@Autowired
-	private FoodService service;
+	private FoodService foodService;
+	
+	@Autowired
+	private BoardService boardService;
 
 	@GetMapping("foodlist.do")
 	public Map<String, Object> getFoods() {
 		List<Food> list = null;
 
 		Map<String, Object> map = new HashMap<>();
-		list = service.searchAll("", "");
+		list = foodService.searchAll("", "");
 		map.put("list", list);
 		return map;
 	}
@@ -35,9 +46,65 @@ public class KyoungMooController {
 		List<Food> list = null;
 		if(word.equals("*")) {word="";}
 		Map<String, Object> map = new HashMap<>();
-		list = service.searchAll(key, word);
+		list = foodService.searchAll(key, word);
 		map.put("list", list);
 		
 		return map;
 	}
+	
+	/***************************************************************************************/
+	
+	@GetMapping("/board")
+	public ResponseEntity<Object> getBoardList() {
+		try {
+			List<Post> list = boardService.select();
+            return response(list, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            throw e;
+        }
+	}
+	
+	@GetMapping("/board/{no}")
+	public ResponseEntity<Object> getBoardOne(@PathVariable String no) {
+		try {
+			Post post = boardService.select(no);
+            return response(post, HttpStatus.OK);
+        } catch (RuntimeException e) {
+            throw e;
+        }
+	}
+	
+	@PostMapping("/board")
+	public ResponseEntity<Object> insertPost(@RequestBody Post p) {
+		try {
+			boolean isSuccess = boardService.insert(p);
+            return response(isSuccess, HttpStatus.OK);
+        } catch (RuntimeException e) {
+        	throw e;
+        }
+	}
+	
+	@PutMapping("/board")
+	public ResponseEntity<Object> updatePost(@RequestBody Post p) {
+		try {
+			boolean isSuccess = boardService.update(p);
+            return response(isSuccess, HttpStatus.OK);
+        } catch (RuntimeException e) {
+        	throw e;
+        }
+	}
+	
+	@DeleteMapping("/board/{no}")
+	public ResponseEntity<Object> deletePost(@PathVariable String no) {
+		try {
+			boolean isSuccess = boardService.delete(no);
+            return response(isSuccess, HttpStatus.OK);
+        } catch (RuntimeException e) {
+        	throw e;
+        }
+	}
+	
+	private ResponseEntity<Object> response(Object data, HttpStatus status) {
+        return new ResponseEntity<Object>(data, status);
+    }
 }

a4bdd27e84e0a0d72a83acc60c485a19e014f6b0
KM <sodp2@naver.com>
2019-11-22 16:17:50 +0900

    feat: 회원관련 기능 REST로 변경

--- back-end/src/main/java/com/ssafy/controller/HaksuController.java
+++ back-end/src/main/java/com/ssafy/controller/HaksuController.java
@@ -5,12 +5,8 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.ssafy.dto.User;
 import com.ssafy.service.UserService;
-import com.ssafy.util.HashUtil;
 
 @Controller
 public class HaksuController {
@@ -23,76 +19,9 @@ public class HaksuController {
 		return "mainPage";
 	}
 
-	@PostMapping("login.do")
-	public String doLogin(String id, String password, RedirectAttributes redirectAttributes, HttpSession session) {
-		// 1. parameter 검증
-		System.out.println("아이디: "+id+" , 패스워드: "+password);
-		String hashPassword = String.valueOf(HashUtil.hash(password));
-		System.out.println("해시 패스워드 : "+hashPassword);
-		// 2. 모델 연결(생략 --> id=ssafy, pass=1111)
-		User user = userService.login(id, hashPassword);
-		System.out.println("유저: "+user);
-		// 3. view 연결
-		if (user == null) {
-			redirectAttributes.addFlashAttribute("msg", "아이디 또는 패스워드가 다릅니다.");
-		} else {
-			// 성공적으로 로그인 했다면..
-			session.setAttribute("user", user);
-		}
-		return "redirect:main.do";
-	}
-
 	@GetMapping("logout.do")
 	public String doLogout(HttpSession session) {
 		session.invalidate();
 		return "redirect:main.do";
 	}
-
-	@PostMapping("insert.do")
-	public String doSignUp(User user, HttpSession session) {
-		// 2. 비밀번호 해시
-		int hashPassword = HashUtil.hash(user.getPassword());
-		System.out.println(hashPassword);
-		user.setPassword(String.valueOf(hashPassword));
-
-		// 3. view 연결
-		userService.insertUser(user);
-		System.out.println(user);
-		session.setAttribute("user", user);
-		return "redirect:main.do";
-	}
-
-	@PostMapping("update.do")
-	public String update(User user, HttpSession session) {
-		// 1. parameter 검증
-		String hashPass = String.valueOf(HashUtil.hash(user.getPassword()));
-		
-		user.setPassword(hashPass);
-		
-		userService.updateUser(user);
-		
-		session.setAttribute("user", user);
-		return "redirect:main.do";
-	}
-
-	@PostMapping("delete.do")
-	public String doDelete(String userid, HttpSession session) {
-		// 2. 모델 연결(생략 --> id=ssafy, pass=1111)
-		// 3. view 연결
-		User user = userService.findUser(userid);
-		userService.deleteUser(user);
-		System.out.println(user);
-		session.invalidate();
-		return "redirect:mainPage.jsp";
-	}
-	
-	/*
-	 * map.put("/main.do", new SelectController()); map.put("/search.do", new
-	 * SearchController()); map.put("/product_list.do", new
-	 * ProductListController()); map.put("/food_info.do", new FoodInfoController());
-	 * map.put("/login.do", new LoginController()); map.put("/logout.do", new
-	 * LogoutController()); map.put("/update.do", new UpdateController());
-	 * map.put("/insert.do", new InsertController()); map.put("/delete.do", new
-	 * deleteController());
-	 */
 }
--- back-end/src/main/java/com/ssafy/controller/KyoungMooController.java
+++ back-end/src/main/java/com/ssafy/controller/KyoungMooController.java
@@ -18,8 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.dto.Food;
 import com.ssafy.dto.Post;
+import com.ssafy.dto.User;
 import com.ssafy.service.BoardService;
 import com.ssafy.service.FoodService;
+import com.ssafy.service.UserService;
+import com.ssafy.util.HashUtil;
 
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 @RestController
@@ -27,9 +30,12 @@ public class KyoungMooController {
 
 	@Autowired
 	private FoodService foodService;
-	
+
 	@Autowired
 	private BoardService boardService;
+
+	@Autowired
+	private UserService userService;
 
 	@GetMapping("foodlist.do")
 	public Map<String, Object> getFoods() {
@@ -44,67 +50,131 @@ public class KyoungMooController {
 	@GetMapping("search.do/{key}/{word}")
 	public Map<String, Object> Eldyd(@PathVariable String key, @PathVariable String word) {
 		List<Food> list = null;
-		if(word.equals("*")) {word="";}
+		if (word.equals("*")) {
+			word = "";
+		}
 		Map<String, Object> map = new HashMap<>();
 		list = foodService.searchAll(key, word);
 		map.put("list", list);
-		
+
 		return map;
 	}
-	
+
 	/***************************************************************************************/
-	
+
 	@GetMapping("/board")
 	public ResponseEntity<Object> getBoardList() {
 		try {
 			List<Post> list = boardService.select();
-            return response(list, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            throw e;
-        }
+			return response(list, HttpStatus.OK);
+		} catch (RuntimeException e) {
+			throw e;
+		}
 	}
-	
+
 	@GetMapping("/board/{no}")
 	public ResponseEntity<Object> getBoardOne(@PathVariable String no) {
 		try {
 			Post post = boardService.select(no);
-            return response(post, HttpStatus.OK);
-        } catch (RuntimeException e) {
-            throw e;
-        }
+			return response(post, HttpStatus.OK);
+		} catch (RuntimeException e) {
+			throw e;
+		}
 	}
-	
+
 	@PostMapping("/board")
 	public ResponseEntity<Object> insertPost(@RequestBody Post p) {
 		try {
 			boolean isSuccess = boardService.insert(p);
-            return response(isSuccess, HttpStatus.OK);
-        } catch (RuntimeException e) {
-        	throw e;
-        }
+			return response(isSuccess, HttpStatus.OK);
+		} catch (RuntimeException e) {
+			throw e;
+		}
 	}
-	
+
 	@PutMapping("/board")
 	public ResponseEntity<Object> updatePost(@RequestBody Post p) {
 		try {
 			boolean isSuccess = boardService.update(p);
-            return response(isSuccess, HttpStatus.OK);
-        } catch (RuntimeException e) {
-        	throw e;
-        }
+			return response(isSuccess, HttpStatus.OK);
+		} catch (RuntimeException e) {
+			throw e;
+		}
 	}
-	
+
 	@DeleteMapping("/board/{no}")
 	public ResponseEntity<Object> deletePost(@PathVariable String no) {
 		try {
 			boolean isSuccess = boardService.delete(no);
-            return response(isSuccess, HttpStatus.OK);
-        } catch (RuntimeException e) {
-        	throw e;
-        }
+			return response(isSuccess, HttpStatus.OK);
+		} catch (RuntimeException e) {
+			throw e;
+		}
 	}
-	
+
+	/*****************************************************************************/
+
+	// login -> ??
+	@PostMapping("/login")
+	public ResponseEntity<Object> doLogin(@RequestBody User user) {
+		// 1. parameter 검증
+		String hashPassword = String.valueOf(HashUtil.hash(user.getPassword()));
+		System.out.println("해시 패스워드 : " + hashPassword);
+		// 2. 모델 연결(생략 --> id=ssafy, pass=1111)
+		User result = userService.login(user.getId(), hashPassword);
+
+		// 3. view 연결
+		if (result == null) {
+			return response(null, HttpStatus.BAD_REQUEST);
+		} else {
+			// 성공적으로 로그인 했다면..
+			return response(result, HttpStatus.OK);
+		}
+	}
+
+	// insert.do -> user
+	@PostMapping("/user")
+	public ResponseEntity<Object> doSignUp(@RequestBody User user) {
+		// 2. 비밀번호 해시
+		int hashPassword = HashUtil.hash(user.getPassword());
+		user.setPassword(String.valueOf(hashPassword));
+
+		// 3. view 연결
+		/* TODO: 아직 무엇을 넘길지 정하지 못함 */
+		try {
+			userService.insertUser(user);
+			return response(user, HttpStatus.OK);
+		} catch (Exception e) {
+			return response(e, HttpStatus.CONFLICT);
+		}
+	}
+
+	// update.do -> user
+	@PutMapping("/user")
+	public ResponseEntity<Object> update(User user) {
+		// 1. parameter 검증
+		String hashPass = String.valueOf(HashUtil.hash(user.getPassword()));
+
+		user.setPassword(hashPass);
+
+		userService.updateUser(user);
+
+		/* TODO: 아직 무엇을 넘길지 정하지 못함 */
+		return response(true, HttpStatus.OK);
+	}
+
+	// delete.do -> user
+	@DeleteMapping("user")
+	public ResponseEntity<Object> doDelete(String userid) {
+		User user = userService.findUser(userid);
+		userService.deleteUser(user);
+
+		// TODO: 세션 초기화 하자
+		// session.invalidate();
+		return response(true, HttpStatus.OK);
+	}
+
 	private ResponseEntity<Object> response(Object data, HttpStatus status) {
-        return new ResponseEntity<Object>(data, status);
-    }
+		return new ResponseEntity<Object>(data, status);
+	}
 }
